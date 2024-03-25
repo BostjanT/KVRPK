ced94c4f75b23a85769c88a26a90bacd8679c3d2
BostjanT <bostjan.gartnar@gmail.com>
2024-03-25 18:40:02 +0100

    Adding some new components for the website

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Facebook from '@/public/icon-facebook.svg'
 import Logo from '@/public/KVRPK_LOGO_small.png'
 import Image from 'next/image'
+import { PhoneIcon, AtSymbolIcon, HomeIcon } from '@heroicons/react/24/outline'
 
 const Footer = () => {
     return (
@@ -30,55 +31,21 @@ const Footer = () => {
                         Šolanje
                     </a>
                 </div>
-                {/* <!-- Klubski podatkir --> */}
+                {/* <!-- Klubski podatki --> */}
                 <div className="flex flex-col items-center justify-between space-y-4 text-lg font-light  text-white">
                     <div className="flex items-center text-white">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            className="w-6 h-6 mr-4">
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
-                            />
-                        </svg>
-                        +386 41 855 924
+                        <PhoneIcon className="h-6 w- mr-4" />
+                        <p>+386 41 855 924</p>
                     </div>
                     <div className="flex items-center text-white">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            className="w-6 h-6 mr-4">
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25"
-                            />
-                        </svg>
-                        info.kvrp.kranj@gmail.com
+                        <AtSymbolIcon className="h-6 w-6 mr-4" />
+                        <p>info.kvrp.kranj@gmail.com</p>
                     </div>
                     <div className="flex items-center text-white">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            className="w-6 h-6 mr-4">
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                            />
-                        </svg>
-                        Vidmarjeva 3, Kranj Slovenija
+                        <HomeIcon className="h-6 w-6 mr-4" />
+                        <p className="text-center">
+                            Vidmarjeva 3, Kranj Slovenija
+                        </p>
                     </div>
                 </div>
 
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,48 +1,50 @@
 import Link from 'next/link'
 import Logo from '../public/KVRPK_LOGO_small.png'
 import Image from 'next/image'
+import Hamburger from './HamburgerMenu'
 
 const Navigation = () => {
     return (
-        <header className="bg-lightBlue relative flex items-center justify-between px-6 py-2 text-center h-50 md:h-[3rem]">
+        <nav className="bg-lightBlue relative flex items-center justify-between px-6 py-2 text-center h-50 md:h-[3rem]">
             <div>
                 <Link href="/">
                     {
                         <Image
                             src={Logo}
-                            className="md:absolute h-10 w-10 md:h-20 md:w-20 top-0  md:top-3"
+                            className="md:absolute block z-30 h-10 w-10 md:h-20 md:w-20 top-0  md:top-3"
                             alt="klubski logotip"></Image>
                     }
                 </Link>
             </div>
-            <div className="flex items-center justify-center space-x-4 md:space-x-8">
+            <div className="hidden md:flex items-center justify-center space-x-4 md:space-x-8">
                 <Link
                     href="/klub"
                     className="hover:text-darkGrey hover:scale-105">
                     O nas
                 </Link>
-                <a
+                <Link
                     href="#resevalni"
                     className="hover:text-darkGrey hover:scale-105">
                     Reševalni pes
-                </a>
-                <a
+                </Link>
+                <Link
                     href="#novice"
                     className="hover:text-darkGrey hover:scale-105">
                     Novice
-                </a>
-                <a
+                </Link>
+                <Link
                     href="#solanje"
                     className="hover:text-darkGrey hover:scale-105">
                     Šolanje
-                </a>
-                <a
+                </Link>
+                <Link
                     href="#galerija"
                     className="hover:text-darkGrey hover:scale-105 pr-10">
                     Galerija
-                </a>
+                </Link>
             </div>
-        </header>
+            <Hamburger />
+        </nav>
     )
 }
 
