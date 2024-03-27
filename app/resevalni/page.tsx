27e0d81dce4c2a0da9d12414a8ba217167975706
BostjanT <bostjan.gartnar@gmail.com>
2024-03-27 18:54:37 +0100

    adding new components and sroll to top btn

--- app/resevalni/page.tsx
+++ app/resevalni/page.tsx
@@ -6,14 +6,14 @@ const ResevalniPsi = () => {
         <section className="flex flex-col container px-5 md:px-0 mt-8 mx-auto min-h-screen shadow-xl shadow-darkGrey relative">
             <div className="bg-hero"></div>
 
-            <div className="flex relative mx-auto items-center w-[1200px] h-[500px] justify-center">
+            <div className="flex md:relative mx-auto items-center max-w-full h-[500px] justify-center">
                 <Image
                     src={RescueDogs}
                     alt="four dogs with maountains in background"
                     className="w-full h-full rounded-md object-center"
                 />
-                <div className="absolute inset-0 bg-darkGrey opacity-30"></div>
-                <div className="absolute -bottom-20 bg-darkGrey w-3/4 max-h-2/5 py-10 mx-auto text-center rounded-lg text-whiter shadow-xl shadow-dark">
+                <div className="absolute inset-0 bg-darkGrey opacity-35"></div>
+                <div className="md:absolute flex flex-col md:-bottom-20 bg-darkGrey w-3/4 max-h-2/5 py-10 mx-auto text-center rounded-lg text-whiter shadow-xl shadow-dark">
                     <h2 className="mb-8 text-whiter">Reševalni psi</h2>
                     <p className="max-w-xl px-4 mx-auto mb-8">
                         Za reševalnega psa so najbolj primerne pasme srednje
