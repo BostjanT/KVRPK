14d79f49c95ce1a86603d94cd69b30a8a9d66029
BostjanT <bostjan.gartnar@gmail.com>
2024-04-04 19:37:38 +0200

    now its done

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import { Metadata } from 'next'
 import Hero from '@/components/Hero'
 import Tabs from '@/components/Tabs'
 import Faq from '@/components/Faq'
-import Gallery from '@/components/Galerry'
 import HomeNews from '@/components/HomeNews'
 import Solanje from '@/components/Solanje'
 import Sponzorji from '@/components/Sponzorji'
+import HomeGallery from '@/components/HomeGallery'
 
 export const metadata: Metadata = {
     title: 'Klub vodnikov reševalnih psov Kranj',
@@ -18,7 +18,7 @@ export default function Home() {
             <Tabs />
             <HomeNews />
             <Faq />
-            <Gallery images={[]} />
+            <HomeGallery />
             <Solanje />
             <Sponzorji />
         </>
